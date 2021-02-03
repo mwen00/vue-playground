3c3b82bfd3232360d3ddae49c76675b60d98dbc5
Melanie Wen <melanie.a.wen@gmail.com>
2021-02-03 18:25:03 -0500

    Add Dyanmic Routing for Event Details

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -13,5 +13,9 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get('/events')
+  },
+
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
   }
 }
